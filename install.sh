982b08c095bfcf4376f976d1750f93138a383f9d
Sandro CAZZANIGA <cazzaniga.sandro@gmail.com>
2020-08-06 07:24:29 +0200

    more logical installation

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ then
   exit 1
 fi
 
+sudo pip install -r requirements.txt
+sudo mkdir -p /opt/bin/
+sudo cp bin/feedly2s3.py /opt/bin/feedly2s3
+
 echo -n "Feedly's API token: "
 read token
-sed -i "s/TOKEN =/TOKEN = "\"$token\""/g" bin/feedly2s3.py
+sudo sed -i "s/TOKEN =/TOKEN = "\"$token\""/g" /opt/bin/feedly2s3
 
 echo -n "AWS S3 bucket's name: "
 read s3name
-sed -i "s/BUCKET =/BUCKET = "\"$s3name\""/g" bin/feedly2s3.py
-
-sudo pip install -r requirements.txt
-sudo mkdir -p /opt/bin/
-sudo mv bin/feedly2s3.py /opt/bin/feedly2s3
+sudo sed -i "s/BUCKET =/BUCKET = "\"$s3name\""/g" /opt/bin/feedly2s3
 
 echo "Done, don't forget to add /opt/bin to your PATH."
